6dfebf977982a95447da23bf9046f45d049eb540
Chris <calutz1125@gmail.com>
2017-02-22 15:35:57 -0800

    Initial Dev_C Commit

---
+++ Assets/Scripts/Menus/InGameMenus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenus : MonoBehaviour
+{
+    public void LoadSceneOnSelect()
+    {
+        SceneManager.LoadScene("Dev-Chris-Menus");
+    }
+}
