199fabe0627a6e223c8418a41cf3de452a41fac9
No-Face-the-3rd <foodflare@yahoo.com>
2017-02-23 09:32:23 -0800

    Added even more of the more of the things

---
+++ Neon Outlaws/Neon Outlaws/Assets/Scripts/Menus/MainMenus.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject optionsMenuPanel;
+    public GameObject soundMenuPanel;
+
+#region Main Menu
+    // Load the development fight scene
+    public void LoadSceneOnSelect()
+    {
+        SceneManager.LoadScene("Dev-Jacob-controls");
+    }
+
+    // Deactivate main panel, bring up the options panel
+    public void LoadOptionsPanel()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(true);
+    }
+
+    // If playing through the Unity Editor, exit play mode. Otherwise exit to desktop
+    public void QuitOnSelect()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    #endregion
+
+#region Options Menu
+    // Deactivate options panel, load sound panel
+    public void LoadSoundPanel()
+    {
+        mainMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        soundMenuPanel.SetActive(true);
+    }
+
+    // Deactivate options menu, activate main panel
+    public void LoadMenuPanel()
+    {
+        optionsMenuPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+    #endregion
+
+#region AudioMenu
+    // Deactivate audio panel, bring up the options panel
+    public void LoadOptionsPanelAudio()
+    {
+        soundMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(true);
+    }
+
+#endregion
+
+}
---
+++ Neon Outlaws/Neon Outlaws/Assets/Scripts/Menus/VolumeSlider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSlider : MonoBehaviour
+{
+    public Slider slider;
+    public AudioSource[] audioSources;
+
+    public void VolumeSliderController()
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            audioSource.volume = slider.value;
+            if (audioSource.volume < slider.value)
+                audioSource.volume = audioSource.volume;
+        }
+    }
+}
