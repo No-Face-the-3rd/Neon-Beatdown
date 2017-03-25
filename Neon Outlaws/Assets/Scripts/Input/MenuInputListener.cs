880e928a7d39490ff7c1444b0ceddbb11abb624e
No-Face-the-3rd <foodflare@yahoo.com>
2017-03-24 17:39:19 -0700

    the things

---
+++ Neon Outlaws/Assets/Scripts/Input/MenuInputListener.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputNew;
+
+public class MenuInputListener : MonoBehaviour {
+    public PlayerInput pInput;
+
+    public AxisAction horizNavAction;
+    public AxisAction vertNavAction;
+    public ButtonAction acceptAction;
+    public ButtonAction declineAction;
+    public ButtonAction resumeAction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
