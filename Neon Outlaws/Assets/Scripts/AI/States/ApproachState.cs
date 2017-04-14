22f265785cad98f12116664f294d6830925385b4
RaskoSmash <bmecklenburg16@gmail.com>
2017-04-13 17:55:50 -0700

    Removed asset Started AI

--- Neon Outlaws/Assets/Scripts/AI/States/ApproachState.cs
+++ Neon Outlaws/Assets/Scripts/AI/States/ApproachState.cs
@@ -3,45 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ApproachState : IAIStates {
-    private readonly EnemyAIController enemy;
+    private readonly EnemyAIController self;
     private float    raycastDistance = 10.0f;
 
-    public ApproachState(EnemyAIController ai) {
-        enemy = ai;
+    public ApproachState(EnemyAIController ai)
+    {
+        self = ai;
     }
     
-	public void UpdateState() {
+	public void UpdateState()
+    {
         Approach(); // Move towards opponent
     }
 
-    public void ToApproachState() {
+    public void ToApproachState()
+    {
         Debug.Log("Already approaching");
     }
 
-    public void ToRetreatState() {
-        enemy.currentState = enemy.retreatState;
+    public void ToRetreatState()
+    {
+        self.currentState = self.retreatState;
         Debug.Log("Retreat state active");
     }
 
-    public void ToWaitState() {
-        enemy.currentState = enemy.waitState;
+    public void ToWaitState()
+    {
+        self.currentState = self.waitState;
         Debug.Log("Wait state active");
     }
 
-    private void Approach() {
-        enemy.rb.AddForce(Vector2.left * enemy.inputState.moveX);
-        RaycastHit2D hit;
-        if (enemy.opponent.GetComponent<Rigidbody2D>().position.x > enemy.rb.position.x)
+    private void Approach()
+    {
+        NBCharacterController selfController = self.cbi.controller;
+        NBCharacterController opponent = CharacterLocator.locator.getCharacter((selfController.playerNum % 2) + 1);
+        float approachRange = 8.0f;
+        if(Mathf.Abs(selfController.transform.position.x - opponent.transform.position.x) > approachRange)
         {
-            enemy.rb.AddForce(Vector2.right * enemy.speed);
-
-            hit = Physics2D.Raycast(enemy.rb.position, Vector2.right, raycastDistance);
+            self.inputState.moveX = selfController.transform.localScale.x;
         }
         else
         {
-            enemy.rb.AddForce(Vector2.left * enemy.speed);
-
-            hit = Physics2D.Raycast(enemy.rb.position, Vector2.left, raycastDistance);
+            self.inputState.moveX = -selfController.transform.localScale.x;
         }
+
+        //RaycastHit2D hit;
+        //if (self.opponent.GetComponent<Rigidbody2D>().position.x > self.rb.position.x)
+        //{
+        //    hit = Physics2D.Raycast(self.rb.position, Vector2.right, raycastDistance);
+        //    if(hit.distance > 3)
+        //    {
+        //        self.inputState.moveX = 1;
+        //    }
+        //    else
+        //    {
+        //        //ToRetreatState();
+        //    }
+        //}
+        //else
+        //{
+        //    hit = Physics2D.Raycast(self.rb.position, Vector2.left, raycastDistance);
+        //    if(hit.distance > 3)
+        //    {
+        //        self.inputState.moveX = -1;
+        //    }
+        //    else
+        //    {
+        //        //ToRetreatState();
+        //    }
+        //}
+        //Debug.Log(hit.distance);
     }
 }
