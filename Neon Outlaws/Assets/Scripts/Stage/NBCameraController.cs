b2862ff6eb4245eb52ab2359851e3e7758ba4426
No-Face-the-3rd <foodflare@yahoo.com>
2017-04-20 12:07:39 -0700

    device mapping and camera controller

--- Neon Outlaws/Assets/Scripts/Stage/NBCameraController.cs
+++ Neon Outlaws/Assets/Scripts/Stage/NBCameraController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class NBCameraController : MonoBehaviour {
 
     public float minX = 0.0f, maxX = 0.0f;
-    public float viewportEdgeBuffer = 0.0f;
+    public float viewportEdgeBuffer = 0.1f;
 
     public int num = 1;
 
     public float curMinX = 0.0f, curMaxX = 0.0f;
+
+    public Vector3 pos;
 
     private Camera mainCam;
 
@@ -20,14 +22,44 @@ public class NBCameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        GameObject character = CharacterLocator.locator.getCharacter(num).gameObject;
-        float zDist = Mathf.Abs(character.transform.position.z - transform.position.z);
-        Vector3 outPos = mainCam.ViewportToWorldPoint(new Vector3(0.0f, 0.5f, zDist));
-        curMinX = outPos.x;
-        outPos = mainCam.ViewportToWorldPoint(new Vector3(1.0f, 0.5f, zDist));
-        curMaxX = outPos.x;
-        mainCam.WorldToViewportPoint(character.transform.position);
-
-
+        NBCharacterController charControl = CharacterLocator.locator.getCharacter(num);
+        if (charControl != null)
+        {
+            GameObject character = charControl.gameObject;
+            float zDist = Mathf.Abs(character.transform.position.z - transform.position.z);
+            Vector3 outPos = mainCam.ViewportToWorldPoint(new Vector3(0.0f, 0.5f, zDist));
+            curMinX = outPos.x;
+            outPos = mainCam.ViewportToWorldPoint(new Vector3(1.0f, 0.5f, zDist));
+            curMaxX = outPos.x;
+            pos = mainCam.WorldToViewportPoint(character.transform.position);
+            if (pos.x < viewportEdgeBuffer || pos.x > 1.0f - viewportEdgeBuffer)
+            {
+                character.transform.position = mainCam.ViewportToWorldPoint(new Vector3(viewportEdgeBuffer, pos.y, zDist));
+                charControl.setState(CharacterState.Idle);
+                charControl.doIdle();
+            }
+        }
     }
+
+    public void checkPan()
+    {
+        int numChars = CharacterLocator.locator.getNumCharacters();
+        List<ControlEdge> edgeCharacters = new List<ControlEdge>();
+        bool leftEdge = false, rightEdge = false;
+        for (int i = 0; i < numChars; i++)
+        {
+            int playerNum = i + 1;
+            NBCharacterController character = CharacterLocator.locator.getCharacter(playerNum);
+            if(character != null)
+            {
+
+            }
+        }
+    }
+}
+
+struct ControlEdge
+{
+    public NBCharacterController character;
+    public bool left;
 }
