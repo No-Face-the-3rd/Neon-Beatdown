dd90c7ca15e263ac810b2f05920a2ac156ffc258
No-Face-the-3rd <foodflare@yahoo.com>
2017-04-19 17:44:25 -0700

    worked on camera controller

---
+++ Neon Outlaws/Assets/Scripts/Stage/NBCameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NBCameraController : MonoBehaviour {
+
+    public float minX = 0.0f, maxX = 0.0f;
+    public float viewportEdgeBuffer = 0.0f;
+
+    public int num = 1;
+
+    public float curMinX = 0.0f, curMaxX = 0.0f;
+
+    private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject character = CharacterLocator.locator.getCharacter(num).gameObject;
+        float zDist = Mathf.Abs(character.transform.position.z - transform.position.z);
+        Vector3 outPos = mainCam.ViewportToWorldPoint(new Vector3(0.0f, 0.5f, zDist));
+        curMinX = outPos.x;
+        outPos = mainCam.ViewportToWorldPoint(new Vector3(1.0f, 0.5f, zDist));
+        curMaxX = outPos.x;
+        mainCam.WorldToViewportPoint(character.transform.position);
+
+
+    }
+}
