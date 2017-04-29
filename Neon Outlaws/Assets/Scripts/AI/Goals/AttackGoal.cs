f30996a3704a2a49004540f0e3738f896210da04
RaskoSmash <bmecklenburg16@gmail.com>
2017-04-28 17:54:56 -0700

    Started attack test goal

--- Neon Outlaws/Assets/Scripts/AI/Goals/AttackGoal.cs
+++ Neon Outlaws/Assets/Scripts/AI/Goals/AttackGoal.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackGoal : MonoBehaviour {
+public class AttackGoal : BaseGoal
+{
+    //test class for attacking
+    public AnimationCurve desireToAttack;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public AnimationCurve desireToAttackW;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        myValues.input = InputTarget.attack;
+        desireToAttackW.keys[0].time = desireToAttack.keys[0].time;
+        desireToAttackW.keys[desireToAttackW.keys.Length - 1].time = desireToAttack[desireToAttack.keys.Length - 1].time;
+    }
+
+    public override void evaluateGoal()
+    {
+        //temp awake fix
+        if (self.cil != null && self.enemyController != null)
+        {
+
+        }
+
+    }
 }
