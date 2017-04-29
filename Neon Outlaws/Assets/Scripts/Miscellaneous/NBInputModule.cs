1ea96808e262fbaf33af007d3e5ab19eb8d122d0
Chris <calutz1125@gmail.com>
2017-04-28 17:43:09 -0700

    New Input Module???

---
+++ Neon Outlaws/Assets/Scripts/Miscellaneous/NBInputModule.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NBInputModule :  BaseInputModule {
+    
+	
+	public override void Process () {
+		
+	}
+}
