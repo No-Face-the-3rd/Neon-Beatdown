449e56e8d230d17f951073bae68a4ab4acd4cbc5
Chris <calutz1125@gmail.com>
2017-05-19 10:28:22 -0700

    Revert "Cleaned up (hopefully?) character select code"
    This reverts commit 1baa846c3d58a373c44692ed6c044ae667a171c9.

--- Neon Outlaws/Assets/Scripts/Menus/StageSelectMenu.cs
+++ Neon Outlaws/Assets/Scripts/Menus/StageSelectMenu.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StageSelectMenu : MonoBehaviour {
-    public TitleMenu titleMenu;
+    TitleMenu titleMenu;
     menuInputState inputState;
     MenuInputListener menuInputListener;
-    //public ObjectDB oDB;
-    
+    public ObjectDB oDB;
+
     public GameObject stageSelectPanel;
     public GameObject characterSelectPanel;
-    public GameObject[] stageButtons;       // Selectable images (down below)
-    public GameObject[] stagePreviewImages; // Top, larger images
+    public GameObject[] stageButtons; // Selectable images (down below)
+    public GameObject[] stagePreviewImages; // Large images
+    int index = 0;
 
     void Awake() {
         //stagePreviewImages[index].SetActive(true);
@@ -25,9 +26,24 @@ public class StageSelectMenu : MonoBehaviour {
             menuInputListener = PlayerLocator.locator.getMenuListener(1);
         }
 
-        //if (inputState.accept.wasPressed)
-        //    if (oDB.selectedStage == 2)
-        //        oDB.selectedStage = Random.Range(0, 2); 
+        if (inputState.accept.wasPressed) {
+            if (oDB.selectedStage == 2)
+                oDB.selectedStage = Random.Range(0, 2);
+
+            for (index = 0; index < stagePreviewImages.Length; index++) {
+                if (titleMenu.menuEventSystem.currentSelectedGameObject == stageButtons[0])
+                    index = 0;
+                else if (titleMenu.menuEventSystem.currentSelectedGameObject == stageButtons[1])
+                    index = 1;
+                else if (titleMenu.menuEventSystem.currentSelectedGameObject == stageButtons[2])
+                    index = 2;
+            }
+
+            if (index < 0)
+                index = stagePreviewImages.Length;
+            if (index > stagePreviewImages.Length)
+                index = 0;
+        }
     }
 
     void TakeInput(menuInputState theMenuInputState) {
--- Neon Outlaws/Assets/Scripts/Menus/TitleMenu.cs
+++ Neon Outlaws/Assets/Scripts/Menus/TitleMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TitleMenu : MonoBehaviour {
     public MainMenu mainMenuButtons;
     MenuInputListener menuInputListener;
