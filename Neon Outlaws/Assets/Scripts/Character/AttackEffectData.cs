8d33b56a778b80f4576e421d794e572d2ed2c962
No-Face-the-3rd <foodflare@yahoo.com>
2017-06-02 15:37:14 -0700

    some tings

--- Neon Outlaws/Assets/Scripts/Character/AttackEffectData.cs
+++ Neon Outlaws/Assets/Scripts/Character/AttackEffectData.cs
@@ -9,17 +9,19 @@ public struct EffectZone
     public Vector2 max;
 }
 
+
 public enum effectType
 {
-    None = 0,
     Knockback = 1 << 0,
     Stun = 1 << 1,
-    Down = 1 << 2
+    Down = 1 << 2,
+    Death = 1 << 3
 }
 
 public class AttackEffectData : MonoBehaviour {
     public Vector2 offset;
     public EffectZone zone;
+    [EnumMask]
     public effectType type;
 
 	// Use this for initialization
